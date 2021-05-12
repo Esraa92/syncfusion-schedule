bfe66e9351b2a91121417145094755b7af20360d
Esraa92 <esraa@hotmail.es>
2021-05-12 10:34:53 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService,
+   AgendaService, ResizeService, DragAndDropService, View, EventRenderedArgs } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'app-root',
+  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'schedule-app';
+  public currentView: View = 'Week';
+  public selectedDate: Date = new Date(2021, 4, 11);
+
+  public data: object[] = [
+    {
+    Id: 1,
+    Subject: 'Meeting',
+    StartTime: new Date(2021, 4, 11, 12, 0),
+    EndTime: new Date(2021, 4, 11, 13, 0)
+      },
+      {
+        Id: 2,
+        Subject: 'Primera visita',
+        StartTime: new Date(2021, 4, 11, 15, 10),
+        EndTime: new Date(2021, 4, 11, 15, 45),
+        CategoryColor: '#0dcaf0'
+          },
+          {
+            Id: 3,
+            Subject: 'Higienes',
+            StartTime: new Date(2021, 4, 12, 10, 0),
+            EndTime: new Date(2021, 4, 12, 10, 0),
+            CategoryColor: '#d63384'
+              }
+      
+    ];
+      public eventSettings: EventSettingsModel = {
+    dataSource: this.data
+      }
+
+      oneventRendered(args: EventRenderedArgs): void {
+        let categoryColor: string = args.data.CategoryColor as string;
+        if (!args.element || !categoryColor) {
+            return;
+        }
+        if (this.currentView === 'Agenda') {
+            (args.element.firstChild as HTMLElement).style.borderLeftColor = categoryColor;
+        } else {
+            args.element.style.backgroundColor = categoryColor;
+        }
+    }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ScheduleAllModule, RecurrenceEditorAllModule } from '@syncfusion/ej2-angular-schedule';
+import { ToolbarAllModule, ContextMenuAllModule } from '@syncfusion/ej2-angular-navigations';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScheduleAllModule,
+    RecurrenceEditorAllModule,
+    ToolbarAllModule, ContextMenuAllModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
